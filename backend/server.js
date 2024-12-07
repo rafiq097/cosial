9ae3f957b4ebdc8a855f7ad423d6ec04704f53dd
Rafiq Shaik <rafiqshaik097@gmail.com>
2024-12-07 19:56:12 +0530

    dist

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import cors from "cors";
+import path from "path";
 
 import connectDB from "./db/db.js";
 import userRoutes from "./routes/user.routes.js";
@@ -17,6 +19,14 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+
+// Cors
+app.use(cors({
+    origin: ['https://cosial.onrender.com', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Routes
 app.use("/api/users", userRoutes);
@@ -26,6 +36,10 @@ cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
